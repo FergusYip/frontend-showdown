b1a8eede33f66ed69e91b141ed408302478b0637
Fergus Yip <contact@fergusyip.me>
2021-11-25 10:30:37 +1100

    Fix form cancel

--- packages/remix/app/routes/home/post.tsx
+++ packages/remix/app/routes/home/post.tsx
@@ -88,7 +88,7 @@ export default function Index() {
   return (
     <div className="my-4">
       <div className="w-full rounded-md border p-4 shadow-md">
-        <Form method="post">
+        <Form id="new-post" method="post">
           <label>
             <h2 className="text-lg font-semibold mb-4">New Post</h2>
             <textarea
@@ -101,21 +101,22 @@ export default function Index() {
             />
             {error && <div className="text-red-500 px-1">{error}</div>}
           </label>
-          <div className="flex justify-end mt-2 space-x-2">
-            <a href="/home">
-              <button className="text-base py-2 px-4 bg-gray-200 text-gray-700 font-semibold rounded-lg shadow-md hover:bg-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-500 focus:ring-offset-2 focus:ring-offset-gray-200">
-                Cancel
-              </button>
-            </a>
-            <button
-              className="text-base py-2 px-6 bg-indigo-600 text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-indigo-200 disabled:opacity-75"
-              type="submit"
-              disabled={Boolean(transition.submission)}
-            >
-              {transition.submission ? "Posting..." : "Post"}
-            </button>
-          </div>
         </Form>
+        <div className="flex justify-end mt-2 space-x-2">
+          <a href="/home">
+            <button className="text-base py-2 px-4 bg-gray-200 text-gray-700 font-semibold rounded-lg shadow-md hover:bg-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-500 focus:ring-offset-2 focus:ring-offset-gray-200">
+              Cancel
+            </button>
+          </a>
+          <button
+            className="text-base py-2 px-6 bg-indigo-600 text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-indigo-200 disabled:opacity-75"
+            type="submit"
+            disabled={Boolean(transition.submission)}
+            form="new-post"
+          >
+            {transition.submission ? "Posting..." : "Post"}
+          </button>
+        </div>
       </div>
     </div>
   );
