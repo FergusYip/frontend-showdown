7c095880377165183bfc9756eac7593d71ec357d
Fergus Yip <contact@fergusyip.me>
2021-11-26 20:23:49 +1100

    allow post editing on profile page

--- app/routes/user/$username.tsx
+++ app/routes/user/$username.tsx
@@ -2,15 +2,24 @@ import { Post, User } from "@prisma/client";
 import { LoaderFunction, MetaFunction, redirect, useLoaderData } from "remix";
 import PostCard from "../../components/PostCard";
 import { db } from "~/utils/db.server";
+import { authenticator } from "../../auth.server";
 
-type LoaderData = { profile: User & { posts: (Post & { User: User })[] } };
+type LoaderData = {
+  profile: User & {
+    posts: (Post & {
+      User: User;
+    })[];
+  };
+  user?: User;
+};
 
 export let loader: LoaderFunction = async ({ params, request }) => {
   const username = params.username;
   if (!username) {
     return redirect("/");
   }
-  const profile = await db.user.findUnique({
+
+  const getProfile = db.user.findUnique({
     where: {
       username,
     },
@@ -29,36 +38,38 @@ export let loader: LoaderFunction = async ({ params, request }) => {
       },
     },
   });
+
+  const [profile, user] = await Promise.all([getProfile, authenticator.isAuthenticated(request)]);
+
   if (!profile) {
     return redirect("/");
   }
 
   const data: LoaderData = {
     profile,
+    user: user ?? undefined,
   };
   return data;
 };
 
 // https://remix.run/api/conventions#meta
 export let meta: MetaFunction = ({ data }) => {
-  const { profile } = data as LoaderData
+  const { profile } = data as LoaderData;
   return {
     title: `${profile.username} @ Ritter`,
     description: `Ritter profile of ${profile.username}`,
   };
 };
 
-interface Props { }
-
-const User = (props: Props) => {
-  const { profile } = useLoaderData<LoaderData>();
+const User = () => {
+  const { profile, user } = useLoaderData<LoaderData>();
 
   return (
     <main>
       <h1 className="text-2xl mb-4 border-b pb-4">{profile.username}</h1>
       <div className="space-y-4">
         {profile.posts.map((post) => (
-          <PostCard key={post.id} post={post} />
+          <PostCard key={post.id} post={post} user={user} />
         ))}
       </div>
     </main>
